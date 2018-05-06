fbbde9821cbfeaa841403e180e685f247f90b9d6
Cameron Kingsbury <cameron@urbint.com>
2018-05-06 13:32:17 -0400

    feat: set max-line-length to 80

--- cameron-doom/+style.el
+++ cameron-doom/+style.el
@@ -7,6 +7,9 @@
 ;; Turn off line wrapping TODO: test
 (setq-default truncate-lines 1)
 
+;; enforce max-line length
+(setq-default fill-column 80)
+
 (add-hook 'before-save-hook 'whitespace-cleanup)
 
 ;; Make line numbers pop - invalid face linum

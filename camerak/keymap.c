ebc94892249cc1e83241dcde9dcbc7255aef14d3
Cameron Kingsbury <cameron@urbint.com>
2018-05-06 12:46:23 -0400

    feat: add wc3 ergodox layer
    Add layer to camerak layout for Warcraft 3, along with a template layer that
    does nothing.

--- camerak/keymap.c
+++ camerak/keymap.c
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
       KC_TRANSPARENT,
                       KC_TRANSPARENT, KC_TRANSPARENT,
-      TO(3), KC_TRANSPARENT, KC_TRANSPARENT),
+      TO(3),          TO(4),          KC_TRANSPARENT),
 
   // SC2 LAYER
   [3] = KEYMAP(
@@ -124,6 +124,79 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO,
              KC_NO,    KC_NO,
       KC_NO, KC_ENTER, KC_BSPACE),
+
+  // WC3 LAYER
+  [4] = KEYMAP(
+      //BATTLE HAND
+      CTL_T(KC_NO),      KC_1,         KC_2,         KC_3,                KC_4,                KC_5,                KC_6,
+      KC_TAB,            KC_Q,         KC_W,         KC_F,                KC_P,                KC_G,                KC_7,
+      LT(1,KC_LBRACKET), KC_A,         KC_R,         KC_S,                KC_T,                KC_D,
+      KC_LSPO,           KC_Z,         KC_X,         KC_C,                KC_V,                KC_B,                KC_8,
+      KC_F1,             KC_F2,        KC_F3,        KC_LALT,             CTL_T(KC_NO),
+
+                                                                          KC_9,                KC_0,
+                                                                                                                    KC_TRANSPARENT,
+                                                                          KC_SPACE,            LT(5,KC_ESCAPE),     KC_LSFT,
+
+
+      //AUXILIARY HAND
+      KC_TRANSPARENT, KC_AUDIO_MUTE,  KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN,  LGUI(KC_KP_PLUS), LGUI(KC_MINUS), TO(0),
+      LGUI(KC_SPACE), KC_J,           KC_L,            KC_U,               KC_Y,             KC_COLN,        KC_DELETE,
+                      KC_H,           KC_N,            KC_E,               KC_I,             KC_O,           LT(1,KC_RBRACKET),
+      KC_TRANSPARENT, KC_K,           KC_M,            KC_COMMA,           KC_DOT,           KC_SLASH,       KC_RSPC,
+                                      KC_LGUI,         KC_LALT,            MEH_T(KC_NO),     ALL_T(KC_NO),   KC_TRANSPARENT,
+
+      KC_TRANSPARENT,
+                      KC_TRANSPARENT, KC_TRANSPARENT,
+      CTL_T(KC_NO),   LT(2,KC_ENTER), KC_BSPACE),
+
+  // WC3 LAYER 2
+  [5] = KEYMAP(
+      //LEFT HAND
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_NO,
+      KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+
+                                  KC_NO, KC_NO,
+                                                KC_NO,
+                                  KC_NO, KC_NO, KC_NO,
+
+
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+
+      KC_NO,
+             KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO),
+
+  // NO LAYER
+  /* [#] = KEYMAP( */
+  /*     //LEFT HAND */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+
+  /*                                 KC_NO, KC_NO, */
+  /*                                               KC_NO, */
+  /*                                 KC_NO, KC_NO, KC_NO, */
+
+
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*            KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+  /*                   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, */
+
+  /*     KC_NO, */
+  /*            KC_NO, KC_NO, */
+  /*     KC_NO, KC_NO, KC_NO, */
 
 };
 

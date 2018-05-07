59237cc499b7daf20c739f354f84177b4c96782c
Cameron Kingsbury <cameron@urbint.com>
2018-05-07 08:19:52 -0400

    feat: add alt+shift
    Add alt+shift to the keyboard layout to expand ergonomics with chunkwm.
    Additionally, replaced transparent keys with no-ops, and formatted.

--- camerak/keymap.c
+++ camerak/keymap.c
@@ -28,135 +28,135 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // MAIN LAYER
   [0] = KEYMAP(
       //LEFT HAND
-      KC_TRANSPARENT,    KC_PGDOWN,    KC_PGUP,      KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC_MEDIA_PLAY_PAUSE, LCTL(KC_Q),
-      KC_TAB,            KC_Q,         KC_W,         KC_F,                KC_P,                KC_G,                KC_TRANSPARENT,
-      LT(1,KC_LBRACKET), KC_A,         KC_R,         KC_S,                KC_T,                KC_D,
-      KC_LSPO,           KC_Z,         KC_X,         KC_C,                KC_V,                KC_B,                KC_CAPSLOCK,
-      KC_TRANSPARENT,    ALL_T(KC_NO), MEH_T(KC_NO), KC_LALT,             KC_LGUI,
+      KC_NO,             KC_PGDOWN,      KC_PGUP,           KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC_MEDIA_PLAY_PAUSE, LCTL(KC_Q),
+      KC_TAB,            KC_Q,           KC_W,              KC_F,                KC_P,                KC_G,                KC_NO,
+      LT(1,KC_LBRACKET), KC_A,           KC_R,              KC_S,                KC_T,                KC_D,
+      KC_LSPO,           KC_Z,           KC_X,              KC_C,                KC_V,                KC_B,                KC_CAPSLOCK,
+      MEH_T(KC_NO),      ALL_T(KC_NO),   LSFT(LALT(KC_NO)), KC_LALT,             KC_LGUI,
 
-                                                                          KC_TRANSPARENT,      KC_TRANSPARENT,
-                                                                                                                    KC_TRANSPARENT,
-                                                                          KC_SPACE,            LT(2,KC_ESCAPE),     CTL_T(KC_NO),
+                                                                                 KC_NO,               KC_NO,
+                                                                                                                           KC_NO,
+                                                                                 KC_SPACE,            LT(2,KC_ESCAPE),     CTL_T(KC_NO)         ,
 
 
       //RIGHT HAND
-      KC_TRANSPARENT, KC_AUDIO_MUTE,  KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN,  LGUI(KC_KP_PLUS), LGUI(KC_MINUS), KC_POWER,
-      LGUI(KC_SPACE), KC_J,           KC_L,            KC_U,               KC_Y,             KC_COLN,        KC_DELETE,
-                      KC_H,           KC_N,            KC_E,               KC_I,             KC_O,           LT(1,KC_RBRACKET),
-      KC_TRANSPARENT, KC_K,           KC_M,            KC_COMMA,           KC_DOT,           KC_SLASH,       KC_RSPC,
-                                      KC_LGUI,         KC_LALT,            MEH_T(KC_NO),     ALL_T(KC_NO),   KC_TRANSPARENT,
+      KC_NO,             KC_AUDIO_MUTE,  KC_AUDIO_VOL_UP,   KC_AUDIO_VOL_DOWN,   LGUI(KC_KP_PLUS),    LGUI(KC_MINUS),      KC_POWER,
+      LGUI(KC_SPACE),    KC_J,           KC_L,              KC_U,                KC_Y,                KC_COLN,             KC_DELETE,
+                         KC_H,           KC_N,              KC_E,                KC_I,                KC_O,                LT(1,KC_RBRACKET),
+      KC_NO,             KC_K,           KC_M,              KC_COMMA,            KC_DOT,              KC_SLASH,            KC_RSPC,
+                         KC_LGUI,        KC_LALT,           LSFT(LALT(KC_NO)),   ALL_T(KC_NO),        MEH_T(KC_NO),
 
-      KC_TRANSPARENT,
-                      KC_TRANSPARENT, KC_TRANSPARENT,
-      CTL_T(KC_NO),   LT(2,KC_ENTER), KC_BSPACE),
+      KC_NO,
+                         KC_NO,          KC_NO,
+      CTL_T(KC_NO),      LT(2,KC_ENTER), KC_BSPACE),
 
   // SYMBOL LAYER
   [1] = KEYMAP(
       //LEFT HAND
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_DLR,         KC_CIRC,        KC_HASH,        KC_UNDS,        KC_TRANSPARENT, KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_KP_PLUS,     KC_KP_ASTERISK, KC_EQUAL,       KC_MINUS,       KC_QUES,
-      KC_LCBR,        KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+      KC_NO,   KC_NO,      KC_NO,          KC_NO,          KC_NO,          KC_NO,     KC_NO,
+      KC_NO,   KC_DLR,     KC_CIRC,        KC_HASH,        KC_UNDS,        KC_NO,     KC_NO,
+      KC_NO,   KC_KP_PLUS, KC_KP_ASTERISK, KC_EQUAL,       KC_MINUS,       KC_QUES,
+      KC_LCBR, KC_LEFT,    KC_DOWN,        KC_UP,          KC_RIGHT,       KC_NO,     KC_NO,
+      KC_NO,   KC_NO,      KC_NO,          KC_NO,          KC_NO,
 
-                                                                      KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                      KC_TRANSPARENT,
-                                                                      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+                                                           KC_NO,          KC_NO,
+                                                                                      KC_NO,
+                                                           KC_NO,          KC_NO,     KC_NO,
 
 
       //RIGHT HAND
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_AT,          KC_PERC,        KC_PIPE,        KC_SCOLON,      KC_TRANSPARENT,
-                      KC_EXLM,        KC_QUOTE,       KC_DQUO,        KC_GRAVE,       KC_TILD,        KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_AMPR,        KC_LABK,        KC_RABK,        KC_BSLASH,      KC_RCBR,
-                                      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+      KC_NO,   KC_NO,      KC_NO,          KC_NO,          KC_NO,          KC_NO,     KC_NO,
+      KC_NO,   KC_NO,      KC_AT,          KC_PERC,        KC_PIPE,        KC_SCOLON, KC_NO,
+               KC_EXLM,    KC_QUOTE,       KC_DQUO,        KC_GRAVE,       KC_TILD,   KC_NO,
+      KC_NO,   KC_NO,      KC_AMPR,        KC_LABK,        KC_RABK,        KC_BSLASH, KC_RCBR,
+                           KC_NO,          KC_NO,          KC_NO,          KC_NO,     KC_NO,
 
-      KC_TRANSPARENT,
-                      KC_TRANSPARENT, KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT),
+      KC_NO,
+               KC_NO,      KC_NO,
+      KC_NO,   KC_NO,      KC_NO),
 
   // NUMBER LAYER
   [2] = KEYMAP(
       //LEFT HAND
-      CHROME_INSPECT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, QMK_LAYOUT,     EMACS_BINDINGS, KC_TRANSPARENT, KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_1,           KC_2,           KC_3,           KC_4,           KC_5,
-      KC_LCTL,        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, EMACS_CONFIG,   KC_TRANSPARENT, KC_TRANSPARENT,
-      KC_TRANSPARENT, ALL_T(KC_NO),   MEH_T(KC_NO),   KC_LALT,        KC_LGUI,
+      CHROME_INSPECT, KC_F1,        KC_F2,        KC_F3,      KC_F4,          KC_F5,        KC_NO,
+      KC_NO,          KC_NO,        KC_NO,        QMK_LAYOUT, EMACS_BINDINGS, KC_NO,        KC_NO,
+      KC_NO,          KC_1,         KC_2,         KC_3,       KC_4,           KC_5,
+      KC_LCTL,        KC_NO,        KC_NO,        KC_NO,      EMACS_CONFIG,   KC_NO,        KC_NO,
+      KC_NO,          ALL_T(KC_NO), MEH_T(KC_NO), KC_LALT,    KC_LGUI,
 
-                                                                      KC_TRANSPARENT, KC_TRANSPARENT,
-                                                                                                      KC_TRANSPARENT,
-                                                                      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+                                                              KC_NO,          KC_NO,
+                                                                                            KC_NO,
+                                                              KC_NO,          KC_NO,        KC_NO,
 
 
       //RIGHT HAND
-      KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_F11,
-      KC_TRANSPARENT, KC_TRANSPARENT, ZSHRC,          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_F12,
-                      KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_TRANSPARENT,
-      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_LCTL,
-                                      KC_LGUI,        KC_LALT,        MEH_T(KC_NO),   ALL_T(KC_NO),   KC_TRANSPARENT,
+      KC_NO,          KC_F6,        KC_F7,        KC_F8,      KC_F9,          KC_F10,       KC_F11,
+      KC_NO,          KC_NO,        ZSHRC,        KC_NO,      KC_NO,          KC_NO,        KC_F12,
+                      KC_6,         KC_7,         KC_8,       KC_9,           KC_0,         KC_NO,
+      KC_NO,          KC_NO,        KC_NO,        KC_NO,      KC_NO,          KC_NO,        KC_LCTL,
+                                    KC_LGUI,      KC_LALT,    MEH_T(KC_NO),   ALL_T(KC_NO), KC_NO,
 
-      KC_TRANSPARENT,
-                      KC_TRANSPARENT, KC_TRANSPARENT,
-      TO(3),          TO(4),          KC_TRANSPARENT),
+      KC_NO,
+                      KC_NO,        KC_NO,
+      TO(3),          TO(4),        KC_NO),
 
   // SC2 LAYER
   [3] = KEYMAP(
       //BATTLE HAND
-      KC_ESCAPE,      KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_6,
-      KC_TAB,         KC_Q,           KC_W,           KC_F,           KC_P,           KC_G,           KC_HOME,
-      KC_LBRACKET,    KC_A,           KC_R,           KC_S,           KC_T,           KC_D,
-      KC_RBRACKET,    KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_QUOTE,
-      MEH_T(KC_NO),   KC_7,           KC_8,           KC_9,           KC_0,
+      KC_ESCAPE,      KC_1,           KC_2,           KC_3,                KC_4,             KC_5,           KC_6,
+      KC_TAB,         KC_Q,           KC_W,           KC_F,                KC_P,             KC_G,           KC_HOME,
+      KC_LBRACKET,    KC_A,           KC_R,           KC_S,                KC_T,             KC_D,
+      KC_RBRACKET,    KC_Z,           KC_X,           KC_C,                KC_V,             KC_B,           KC_QUOTE,
+      MEH_T(KC_NO),   KC_7,           KC_8,           KC_9,                KC_0,
 
-                                                                      LCA_T(KC_NO),   C_S_T(KC_NO),
-                                                                                                      LSFT(LALT_T(KC_NO)),
-                                                                      LCTL_T(KC_NO),  LALT_T(KC_NO),  LSFT_T(KC_NO),
+                                                                           LCA_T(KC_NO),     C_S_T(KC_NO),
+                                                                                                             LSFT(LALT_T(KC_NO)),
+                                                                           LCTL_T(KC_NO),    LALT_T(KC_NO),  LSFT_T(KC_NO),
 
 
       //AUXILIARY HAND
       KC_NO,          KC_AUDIO_MUTE,  KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN,  LGUI(KC_KP_PLUS), LGUI(KC_MINUS), TO(0),
       LGUI(KC_SPACE), KC_J,           KC_L,            KC_U,               KC_Y,             KC_COLN,        KC_DELETE,
                       KC_H,           KC_N,            KC_E,               KC_I,             KC_O,           KC_RBRACKET,
-      KC_NO, KC_K,    KC_M,           KC_COMMA,        KC_DOT,             KC_SLASH,         KC_RSPC,
+      KC_NO,          KC_K,           KC_M,            KC_COMMA,           KC_DOT,           KC_SLASH,       KC_RSPC,
                                       KC_LGUI,         KC_LALT,            MEH_T(KC_NO),     ALL_T(KC_NO),   KC_NO,
 
       KC_NO,
-             KC_NO,    KC_NO,
-      KC_NO, KC_ENTER, KC_BSPACE),
+                      KC_NO,          KC_NO,
+      KC_NO,          KC_ENTER,       KC_BSPACE),
 
   // WC3 LAYER
   [4] = KEYMAP(
       //BATTLE HAND
-      CTL_T(KC_NO),      KC_1,         KC_2,         KC_3,                KC_4,                KC_5,                KC_6,
-      KC_TAB,            KC_Q,         KC_W,         KC_F,                KC_P,                KC_G,                KC_7,
-      LT(1,KC_LBRACKET), KC_A,         KC_R,         KC_S,                KC_T,                KC_D,
-      KC_LSPO,           KC_Z,         KC_X,         KC_C,                KC_V,                KC_B,                KC_8,
-      KC_F1,             KC_F2,        KC_F3,        KC_LALT,             CTL_T(KC_NO),
+      CTL_T(KC_NO),      KC_1,           KC_2,            KC_3,               KC_4,             KC_5,            KC_6,
+      KC_TAB,            KC_Q,           KC_W,            KC_F,               KC_P,             KC_G,            KC_7,
+      LT(1,KC_LBRACKET), KC_A,           KC_R,            KC_S,               KC_T,             KC_D,
+      KC_LSPO,           KC_Z,           KC_X,            KC_C,               KC_V,             KC_B,            KC_8,
+      KC_F1,             KC_F2,          KC_F3,           KC_LALT,            CTL_T(KC_NO),
 
-                                                                          KC_9,                KC_0,
-                                                                                                                    KC_TRANSPARENT,
-                                                                          KC_SPACE,            LT(5,KC_ESCAPE),     KC_LSFT,
+                                                                              KC_9,             KC_0,
+                                                                                                                 KC_NO,
+                                                                              KC_SPACE,         LT(5,KC_ESCAPE), KC_LSFT,
 
 
       //AUXILIARY HAND
-      KC_TRANSPARENT, KC_AUDIO_MUTE,  KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN,  LGUI(KC_KP_PLUS), LGUI(KC_MINUS), TO(0),
-      LGUI(KC_SPACE), KC_J,           KC_L,            KC_U,               KC_Y,             KC_COLN,        KC_DELETE,
-                      KC_H,           KC_N,            KC_E,               KC_I,             KC_O,           LT(1,KC_RBRACKET),
-      KC_TRANSPARENT, KC_K,           KC_M,            KC_COMMA,           KC_DOT,           KC_SLASH,       KC_RSPC,
-                                      KC_LGUI,         KC_LALT,            MEH_T(KC_NO),     ALL_T(KC_NO),   KC_TRANSPARENT,
+      KC_NO,             KC_AUDIO_MUTE,  KC_AUDIO_VOL_UP, KC_AUDIO_VOL_DOWN,  LGUI(KC_KP_PLUS), LGUI(KC_MINUS),  TO(0),
+      LGUI(KC_SPACE),    KC_J,           KC_L,            KC_U,               KC_Y,             KC_COLN,         KC_DELETE,
+                         KC_H,           KC_N,            KC_E,               KC_I,             KC_O,            LT(1,KC_RBRACKET),
+      KC_NO,             KC_K,           KC_M,            KC_COMMA,           KC_DOT,           KC_SLASH,        KC_RSPC,
+                                         KC_LGUI,         KC_LALT,            MEH_T(KC_NO),     ALL_T(KC_NO),    KC_NO,
 
-      KC_TRANSPARENT,
-                      KC_TRANSPARENT, KC_TRANSPARENT,
-      CTL_T(KC_NO),   LT(2,KC_ENTER), KC_BSPACE),
+      KC_NO,
+                         KC_NO,          KC_NO,
+      CTL_T(KC_NO),      LT(2,KC_ENTER), KC_BSPACE),
 
   // WC3 LAYER 2
   [5] = KEYMAP(
       //LEFT HAND
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-      KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_NO, KC_NO,
-      KC_NO, KC_NO, KC_P4, KC_P5, KC_P6, KC_NO,
-      KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_NO, KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_P7, KC_P8, KC_P9, KC_NO,
+      KC_NO, KC_NO, KC_NO, KC_P4, KC_P5, KC_P6,
+      KC_NO, KC_NO, KC_NO, KC_P1, KC_P2, KC_P3, KC_NO,
       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
 
                                   KC_NO, KC_NO,

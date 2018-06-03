bc7670f695ba90f5d3a3d6a259a8f5a9b142a9b7
Cameron Kingsbury <camsbury7@gmail.com>
2018-06-02 23:38:53 -0400

    feat: align org across emacs configs

--- cameron-doom/+org.el
+++ cameron-doom/+org.el
@@ -1,14 +1,15 @@
 (setq org-directory (expand-file-name "~/projects/lxndr/"))
 
-(defun +cameron/org-capture-hook ()
-  (setq org-capture-templates '(("RET" "Push onto Task Stack" entry
-                                 (file+headline "~/projects/lxndr/task_stack.org" "Tasks")
-                                 "* [ ] %i%?"))
 
-        org-agenda-files '("~/projects/lxndr/queue.org")))
-
-(after! org-capture
-  (+cameron/org-capture-hook))
+(setq org-directory (expand-file-name "~/projects/lxndr/")
+      org-capture-templates '(("n" "Place in the Inbox" entry
+                               (file+headline "~/projects/lxndr/inbox.org" "Inbox") "* [ ] %i%?"))
+      org-agenda-files '("~/projects/lxndr/store.org")
+      org-refile-targets '(("~/projects/lxndr/queue.org" :maxlevel . 3)
+                           ("~/projects/lxndr/store.org" :level . 1)
+                           ("~/projects/lxndr/ref.org" :level . 1))
+      org-archive-location (concat "~/projects/lxndr/archive/" (format-time-string "%Y-%m") ".org::")
+      org-todo-keywords '((sequence "[ ]" "[x]")))
 
 (setq org-refile-targets '(("~/projects/lxndr/queue.org" :maxlevel . 3)
                            ("~/projects/lxndr/ref.org" :level . 1)))
